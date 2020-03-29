09135aa4a2fbefd560333521ee552a2cdf864b72
Leon <Leon.ID@QQ.COM>
2020-03-29 21:03:29 +0800

    客户端链接Proto文件，并创建通道和客户端，请求Grpc服务；

--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcServiceDemo.Protos;
 
 namespace GrpcClient
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new EmployeeService.EmployeeServiceClient(channel);
+            var response = await client.GetByNoAsync(new GetByNoRequest() { No = 102 });
+            Console.WriteLine(response.Employee.FirstName);
+            Console.ReadLine();
         }
     }
 }
