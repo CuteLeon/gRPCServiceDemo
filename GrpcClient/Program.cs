6ebd13ea73d9403288abdfe0df8a36e4d7164e40
Leon <Leon.ID@QQ.COM>
2020-03-29 21:46:53 +0800

    实现全部的gRPC接口的逻辑；

--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.IO;
 using System.Threading.Tasks;
+using Google.Protobuf;
 using Grpc.Core;
 using Grpc.Net.Client;
 using GrpcServiceDemo.Protos;
@@ -17,8 +19,57 @@ namespace GrpcClient
             };
             using var channel = GrpcChannel.ForAddress("https://localhost:5001");
             var client = new EmployeeService.EmployeeServiceClient(channel);
+
             var response = await client.GetByNoAsync(new GetByNoRequest() { No = 102 }, metaDatas);
             Console.WriteLine(response.Employee.FirstName);
+
+            using var getAllCall = client.GetAll(new GetAllRequest());
+            var responseStream = getAllCall.ResponseStream;
+            while (await responseStream.MoveNext())
+            {
+                Console.WriteLine(responseStream.Current.Employee.FirstName);
+            }
+
+            FileStream fileStream = File.OpenRead("Dream_x280.jpg");
+            using var uploadPhotoCall = client.UploadPhoto();
+            while (fileStream.Position < fileStream.Length)
+            {
+                var buffer = new byte[1024];
+                int length = await fileStream.ReadAsync(buffer, 0, buffer.Length);
+
+                if (length < buffer.Length)
+                {
+                    Array.Resize(ref buffer, length);
+                }
+
+                await uploadPhotoCall.RequestStream.WriteAsync(
+                    new UploadPhotoRequest() { Data = ByteString.CopyFrom(buffer) });
+            }
+            await uploadPhotoCall.RequestStream.CompleteAsync();
+
+
+            using var saveAllCall = client.SaveAll();
+            for (int index = 0; index < 5; index++)
+            {
+                await saveAllCall.RequestStream.WriteAsync(new EmployeeRequest()
+                {
+                    Employee = new Employee()
+                    {
+                        Id = 10 + index,
+                        FirstName = "Employee",
+                        LastName = index.ToString(),
+                        No = 110 + index,
+                        Salary = 10000 + index * 1000,
+                    }
+                });
+            }
+            await saveAllCall.RequestStream.CompleteAsync();
+
+            while (await saveAllCall.ResponseStream.MoveNext())
+            {
+                Console.WriteLine(saveAllCall.ResponseStream.Current.Employee.Id);
+            }
+
             Console.ReadLine();
         }
     }
--- GrpcServiceDemo/Services/MyEmployeeService.cs
+++ GrpcServiceDemo/Services/MyEmployeeService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Grpc.Core;
@@ -18,7 +19,7 @@ namespace GrpcServiceDemo.Services
             this.logger = logger;
         }
 
-        public override Task<EmployeeResponse> GetByNo(GetByNoRequest request, ServerCallContext context)
+        public override async Task<EmployeeResponse> GetByNo(GetByNoRequest request, ServerCallContext context)
         {
             var metaData = context.RequestHeaders;
             foreach (var pair in metaData)
@@ -29,10 +30,45 @@ namespace GrpcServiceDemo.Services
             var employee = InMemoryData.Employees.SingleOrDefault(employee => employee.No == request.No);
             if (employee != null)
             {
-                return Task.FromResult(new EmployeeResponse() { Employee = employee });
+                return new EmployeeResponse() { Employee = employee };
             }
 
             throw new Exception("Employee Not Found.");
+        }
+
+        public override async Task GetAll(GetAllRequest request, IServerStreamWriter<EmployeeResponse> responseStream, ServerCallContext context)
+        {
+            foreach (var employee in InMemoryData.Employees)
+            {
+                await responseStream.WriteAsync(new EmployeeResponse() { Employee = employee });
+            }
+        }
+
+        public override async Task<UploadPhotoResponse> UploadPhoto(IAsyncStreamReader<UploadPhotoRequest> requestStream, ServerCallContext context)
+        {
+            var memoryStream = new MemoryStream();
+            while (await requestStream.MoveNext())
+            {
+                await memoryStream.WriteAsync(requestStream.Current.Data.ToByteArray());
+            }
+
+            Console.WriteLine(memoryStream.Length);
+            return new UploadPhotoResponse() { Result = true };
+        }
+
+        public override async Task<EmployeeResponse> Save(EmployeeRequest request, ServerCallContext context)
+        {
+            InMemoryData.Employees.Add(request.Employee);
+            return new EmployeeResponse() { Employee = request.Employee };
+        }
+
+        public override async Task SaveAll(IAsyncStreamReader<EmployeeRequest> requestStream, IServerStreamWriter<EmployeeResponse> responseStream, ServerCallContext context)
+        {
+            while (await requestStream.MoveNext())
+            {
+                var response = await Save(requestStream.Current, context);
+                await responseStream.WriteAsync(response);
+            }
         }
     }
 }
