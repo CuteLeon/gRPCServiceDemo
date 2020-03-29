99a25c3d06ddf49da909e600fc408c5cccdcc30a
Leon <Leon.ID@QQ.COM>
2020-03-29 20:49:32 +0800

    设计Employee的消息和服务ProtoBuf文件；并配置proto文件的编译属性；

---
+++ GrpcServiceDemo/Services/MyEmployeeService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using GrpcServiceDemo.Protos;
+using Microsoft.Extensions.Logging;
+
+namespace GrpcServiceDemo.Services
+{
+    public class MyEmployeeService : EmployeeService.EmployeeServiceBase
+    {
+        private readonly ILogger<MyEmployeeService> logger;
+
+        public MyEmployeeService(ILogger<MyEmployeeService> logger)
+        {
+            this.logger = logger;
+        }
+
+        public override Task<EmployeeResponse> GetByNo(GetByNoRequest request, ServerCallContext context)
+        {
+            return base.GetByNo(request, context);
+        }
+    }
+}
